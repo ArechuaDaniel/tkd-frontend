d28ad57b6866540dbfb2bd4bb5f4a033cb7b78a3
ArechuaDaniel <daniel.ar98@hotmai.com>
2024-09-11 18:39:40 -0500

    formulario yup

--- src/modules/admin/views/ProductView.ts
+++ src/modules/admin/views/ProductView.ts
@@ -1,9 +1,28 @@
+import { useFieldArray, useForm } from 'vee-validate';
+
 import { getProductById } from '@/modules/products/actions';
 import { useQuery } from '@tanstack/vue-query';
-import { defineComponent, watchEffect } from 'vue';
+import { defineComponent, watch, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
+import * as yup from 'yup';
+import CustomInput from '@/modules/common/components/CustomInput.vue';
+import CustomTextArea from '@/modules/common/components/CustomTextArea.vue';
+
+
+const validationSchema = yup.object({
+  title: yup.string().required('Este campo es super importante').min(3, 'Mínimo de 3 letras!!!'),
+  slug: yup.string().required(),
+  description: yup.string().required(),
+  price: yup.number().required(),
+  stock: yup.number().required().min(1),
+  gender: yup.string().required().oneOf(['men', 'women', 'kid']),
+});
 
 export default defineComponent({
+  components: {
+    CustomInput, 
+    CustomTextArea,
+  },
   props: {
     productId: { type: String, required: true },
   },
@@ -19,21 +38,91 @@ export default defineComponent({
       queryFn: () => getProductById(props.productId),
       retry: false,
     });
+    const { values, defineField, errors, handleSubmit, resetForm, meta } = useForm({
+      validationSchema,
+      //initialValues: product.value,
+    });
 
+    const [title, titleAttrs] = defineField('title');
+    const [slug, slugAttrs] = defineField('slug');
+    const [description, descriptionAttrs] = defineField('description');
+    const [price, priceAttrs] = defineField('price');
+    const [stock, stockAttrs] = defineField('stock');
+    const [gender, genderAttrs] = defineField('gender');
+
+    const { fields: sizes, remove: removeSize, push: pushSize } = useFieldArray<string>('sizes');
+    const { fields: images } = useFieldArray<string>('images');
+
+    const onSubmit = handleSubmit(async (values) => {
+      console.log(values);
+    });
+
+    const toggleSize = (size: string) => {
+      const currentSizes = sizes.value.map((s) => s.value);
+      const hasSize = currentSizes.includes(size);
+
+      if (hasSize) {
+        removeSize(currentSizes.indexOf(size));
+      } else {
+        pushSize(size);
+      }
+    };
     watchEffect(() => {
-        if (isError.value && !isLoading.value) {
-          router.replace('/admin/products');
-          return;
-        }
-      });
+      if (isError.value && !isLoading.value) {
+        router.replace('/admin/products');
+        return;
+      }
+    });
+    watch(
+      product,
+      () => {
+        if (!product) return;
+
+        resetForm({
+          values: product.value,
+        });
+      },
+      {
+        deep: true,
+        immediate: true,
+      },
+    );
 
     return {
       //Properties
+      values,
+      errors,
+      meta,
+
+      title,
+      titleAttrs,
+      slug,
+      slugAttrs,
+      description,
+      descriptionAttrs,
+      price,
+      priceAttrs,
+      stock,
+      stockAttrs,
+      gender,
+      genderAttrs,
+
+      sizes,
+      removeSize,
+      pushSize,
+      images,
 
       // Getters
       allSizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
 
       // Actions
+      onSubmit,
+      toggleSize,
+
+      hasSize: (size: string) => {
+        const currentSizes = sizes.value.map((s) => s.value);
+        return currentSizes.includes(size);
+      },
     };
   },
 });
